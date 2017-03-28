88a0d2b8365ae0633e837eab53d4c889d50ed86f
Yuki Chiba <archiba@Yuki-no-MacBook-Pro.local>
2017-03-28 16:25:30 +0900

    add hyper config

---
+++ hyper.js
@@ -0,0 +1,21 @@
+module.exports = {
+  config: {
+    fontSize: 12
+    
+    // settings for hyperline
+
+  },
+
+  plugins: [
+    "hyperpower",
+    "hyper-darkmatter",
+    "hyper-statusline",
+    "hyperterm-dibdabs",
+    "hyperlinks",
+    "hyper-blink",
+    "hyperterm-tab-icons"
+  ],
+
+  localPlugins: [
+  ]
+}
